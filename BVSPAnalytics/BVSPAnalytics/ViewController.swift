517e5d55d72dd4c759f7f1fe4dbe03203bbe32f7
Walter Junior <juniordeonisio@gmail.com>
2016-07-04 02:29:56 -0300

    Creation app BVSPAnalytics.

---
+++ BVSPAnalytics/BVSPAnalytics/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  BVSPAnalytics
+//
+//  Created by Walter Junior on 04/07/16.
+//  Copyright © 2016 Walter Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("App Loaded")
+
+        let url = NSURL(string: "http://localhost/#bovespa:EMPRESAS?5B504F5")
+        
+        webView.loadRequest(NSURLRequest(URL: url!))
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
