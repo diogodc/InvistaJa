5c92f3cd8b8ef9fdd8e4fcad73f274686d80a844
rafaelprufino <Keell22*>
2016-10-09 17:22:51 -0300

    Version 5.0.4

--- Modulo_Wbs/Wsb/wsb.php
+++ Modulo_Wbs/Wsb/wsb.php
@@ -1,11 +1,16 @@
 <?php
 session_start();
 
-require_once './App/App.php';
+require_once './App/Features.php';
 
 $App = new \Features();
 
 $App::CopySession();
+
+
+$App->Routing()->Get("/", function() {
+    echo json_encode(array(success => true, error => func_get_args()));
+});
 
 $App->Routing()->Error(function() {
     echo json_encode(array(success => false, error => func_get_args()));
