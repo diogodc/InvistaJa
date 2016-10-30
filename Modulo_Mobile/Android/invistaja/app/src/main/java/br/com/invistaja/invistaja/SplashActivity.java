6627df7d395f95b26d6ecc5a897df329b864f073
ecardoso <everaldocardoso.github@gmail.com>
2016-10-29 23:36:22 -0300

    App

--- Modulo_Mobile/Android/invistaja/app/src/main/java/br/com/invistaja/invistaja/SplashActivity.java
+++ Modulo_Mobile/Android/invistaja/app/src/main/java/br/com/invistaja/invistaja/SplashActivity.java
@@ -1,28 +1,27 @@
 package br.com.invistaja.invistaja;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent actySplash = new Intent(SplashActivity.this,
-
-                        SplashActivity.class);
-
-
-                SplashActivity.this.startActivity(actySplash);
+                Intent splash = new Intent(SplashActivity.this,
+                        PrincipalActivity.class);
+                SplashActivity.this.startActivity(splash);
                 SplashActivity.this.finish();
             }
-        }, 5000);
+        }, 3500);
     }
 }
