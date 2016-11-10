e3cbaf17eaf1dc793f7c33faa58eddd4c66852d4
ecardoso <everaldocardoso.github@gmail.com>
2016-11-10 19:53:21 -0300

    App

---
+++ Modulo_Mobile/Android/invistaja/app/src/main/java/br/com/invistaja/invistaja/view/activitys/CadastroActivity.java
@@ -0,0 +1,15 @@
+package br.com.invistaja.invistaja.view.activitys;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import br.com.invistaja.invistaja.R;
+
+public class CadastroActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
---
+++ Modulo_Mobile/Android/invistaja/app/src/main/java/br/com/invistaja/invistaja/view/activitys/ContatoActivity.java
@@ -0,0 +1,15 @@
+package br.com.invistaja.invistaja.view.activitys;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import br.com.invistaja.invistaja.R;
+
+public class ContatoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contato);
+    }
+}
