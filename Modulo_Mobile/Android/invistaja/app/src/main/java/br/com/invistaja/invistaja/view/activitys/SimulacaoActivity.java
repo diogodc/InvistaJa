68e6755247d3c91d24adacf6690f469c173aadf2
ecardoso <everaldocardoso.github@gmail.com>
2016-11-10 20:59:41 -0300

    App

---
+++ Modulo_Mobile/Android/invistaja/app/src/main/java/br/com/invistaja/invistaja/view/activitys/SimulacaoActivity.java
@@ -0,0 +1,17 @@
+package br.com.invistaja.invistaja.view.activitys;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+import br.com.invistaja.invistaja.R;
+
+public class SimulacaoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.setContentView(R.layout.activity_simulacao);
+    }
+}
