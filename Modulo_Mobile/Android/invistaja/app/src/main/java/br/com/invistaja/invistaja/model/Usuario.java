2af17c9c8cdbdd4078f801ecd58c2a3374ac7c1d
ecardoso <everaldocardoso.github@gmail.com>
2016-11-20 11:23:36 -0300

    App

---
+++ Modulo_Mobile/Android/invistaja/app/src/main/java/br/com/invistaja/invistaja/model/Usuario.java
@@ -0,0 +1,4 @@
+package br.com.invistaja.invistaja.model;
+
+public class Usuario {
+}
---
+++ Modulo_Mobile/Android/invistaja/app/src/main/java/br/com/invistaja/invistaja/repository/CadastrarUsuario.java
@@ -0,0 +1,21 @@
+package br.com.invistaja.invistaja.repository;
+
+import android.os.AsyncTask;
+
+public class CadastrarUsuario extends AsyncTask<Void,Void,Void> {
+
+    @Override
+    protected void onPreExecute(){
+
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void Void){
+
+    }
+}
